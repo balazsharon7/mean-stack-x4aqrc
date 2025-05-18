f54c339a820f24cc81686fa01ec4f7b684587a31
balazsharon7 <132854105+balazsharon7@users.noreply.github.com>
2025-05-18 21:08:55 +0200

    client

--- mean-stack-x4aqrc/server/src/models/like-model.ts
+++
@@ -1,9 +0,0 @@
-import type { ObjectId } from "mongodb"
-
-export interface Like {
-  _id?: ObjectId
-  userId: ObjectId
-  targetType: "post" | "comment"
-  targetId: ObjectId
-  createdAt: Date
-}
